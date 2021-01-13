697e8ddd86af8b2f0c254e53ca02ddd1ce11bfc3
Guurkha <72765550+Guurkha@users.noreply.github.com>
2021-01-13 16:33:05 +0100

    fixed final version

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -297,13 +297,13 @@ bool labmatrix::operator!=(labmatrix &second)
     {
         for(int j = 0; j < b; j++)
         {
-            if(this->get(i, j) != second.get(i, j))
+            if(this->get(i, j) == second.get(i, j))
             {
-                return true;
+                return false;
             }
         }
     }
-    return false;
+    return true;
 }
 
 /*
